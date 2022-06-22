75a44c58ebc7b9c6bdf38986c308eba59b9c809f
Lucas Vieira <lucasvieira.dev2020@gmail.com>
2022-06-22 13:01:14 -0300

    aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import { gql, useQuery } from "@apollo/client"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher {
-        name
-      }
-    }
-  }
-`
+import {Event } from "./Pages/Event"
 
 function App() {
-
-  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
-  interface Lesson {
-    id: string;
-    title: string
-  }
-
   return (
-    <div className="text-5xl font-bold text-violet-500">
-      <ul>
-        {data?.lessons.map(lesson => {
-          return <li key={lesson.id}>{lesson.title}</li>
-        })}
-      </ul>
-    </div>
+    <Event />
   )
 }
 
